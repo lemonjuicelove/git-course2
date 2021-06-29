4345b985763090c0ab23732d8e63e28784437c4b
lemon <lemon@whut.com>
2021-06-29 20:39:03 +0800

    hot-fix first commit

--- src/main/java/com/whut/git/GitTest.java
+++ src/main/java/com/whut/git/GitTest.java
@@ -1,9 +1,12 @@
 package com.whut.git;
 
 public class GitTest {
+
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello github");
         System.out.println("hello gitlab");
+        System.out.println("hello gitee");
     }
+
 }
